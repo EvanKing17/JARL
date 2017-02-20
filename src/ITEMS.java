dc02020f4fc80202e947e85613eb7771ea958600
Abazzi <adambazzi2416@gmail.com>
2017-02-19 22:15:09 -0500

    Added Friendo Weapons

--- src/ITEMS.java
+++ src/ITEMS.java
@@ -7,14 +7,14 @@ public class ITEMS {
     public static final Staff starterStaff = new Staff("Basic Staff", "A very basic staff.", 10);
     public static final Dagger starterDagger = new Dagger("Basic Dagger", "A very basic dagger.", 10);
 
-    public static final String[] swordNames = {"Sword 1", "Sword 2", "Sword3"};
-    public static final String[] swordDescs = {"A cool sword", "A second cool sword", "Third and final sword"};
+    public static final String[] swordNames = {"Blunt Sword", "Lixraika Cutlass", "The Supreme Trident"};
+    public static final String[] swordDescs = {"a near useless blade", "The blade prefered by sailors from the Viryan Coast", "It's the best trident, everybody knows it"};
 
-    public static final String[] daggerNames = {"dagger 1", "dagger 2", "dagger3"};
-    public static final String[] daggerDescs = {"A cool dagger", "A second cool dagger", "Third and final dagger"};
+    public static final String[] daggerNames = {"Saladian Dagger", "dagger 2", "dagger3"};
+    public static final String[] daggerDescs = {"A blade forged in Waldorf, Saladia", "A second cool dagger", "Third and final dagger"};
 
-    public static final String[] staffDescs = {"A cool Staff", "A second cool Staff", "Third and final Staff"};
-    public static final String[] staffNames = {"Staff 1", "Staff 2", "Staff3"};
+    public static final String[] staffDescs = {"The staff of the Legendary Wizard Kossis", "A second cool Staff", "Third and final Staff"};
+    public static final String[] staffNames = {"Staff of Kossis", "Staff 2", "Staff3"};
 
 
     public static Blade generateSword() {
